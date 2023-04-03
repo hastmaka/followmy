5b0eb35e37b59db1e97e7920cd4dc86dff15452c
hastmaka <justforappleidincuba@gmail.com>
2023-04-03 15:03:11 -0700

    custom footer added to grid

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,14 @@
-import {useSelector} from "react-redux";
 import {useRoutes} from 'react-router-dom';
 import Layout from "../layout/Layout";
-import UberAndLyft from "../section/table/uberAndLyft/UberAndLyft";
 import Test from "../section/test/Test";
-import Personal from "../section/table/personal/Personal";
+import {lazy, Suspense} from "react";
+//dynamic import
+const UberAndLyft = lazy(() => import('../section/table/uberAndLyft/UberAndLyft'))
+const Personal = lazy(() => import('../section/table/personal/Personal'))
+
+const FallBack = () => {
+    return <div>Loading...</div>
+}
 
 export default function Router() {
     const user = JSON.parse(localStorage.getItem('user'))
@@ -12,14 +17,14 @@ export default function Router() {
     if(user.selectedValue === 'uber_and_lyft') {
         children.push({
             path: 'uber_and_lyft',
-            element: <UberAndLyft/>
+            element: <Suspense fallback={<FallBack/>}><UberAndLyft/></Suspense>
         })
     }
 
     if(user.selectedValue === 'personal') {
         children.push({
             path: 'personal',
-            element: <Personal/>
+            element: <Suspense fallback={<FallBack/>}><Personal/></Suspense>
         })
     }
 
